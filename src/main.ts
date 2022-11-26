f328f3c962bc6261262fb7efeea605aafb81c935
Elayd <iamvine@mail.ru>
2022-11-26 23:22:07 +0300

    eeq

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 3000 || process.env.PORT
+  const PORT = process.env.PORT || 3333
 
   app.enableCors({
     origin: [/^(.*)/],
